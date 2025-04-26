966d8a2d953d81090480d49a6f40a6980befb3f1
ahmed <ahmedmijdo2@gmail.com>
2025-04-26 21:18:15 +0300

    add hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Hero from './components/Hero';
 import Navbar from './components/Navbar'
 
 function App() {
   const navItems = ['hero', 'about', 'projects', 'contact'];
   return (
     <>
-      <Navbar navItems={navItems}/>
+      <header className="bg-indigo-500 text-white p-2 flex justify-between items-center rounded-b-md">
+        <Navbar navItems={navItems}/>
+      </header>
+      <main>
+        <Hero />
+      </main>
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,20 +14,20 @@ const Navbar = ({navItems}: IProps) => {
     };
 
     return (
-        <header className="bg-indigo-500 text-white p-2 flex justify-between items-center rounded-b-md">
+        <>
             <h1 className="uppercase font-bold cursor-pointer bg-indigo-800 p-2 rounded-md">mijdo</h1>
             <nav>
                 <ul className="flex">
                     {navItems.map(item => 
                         <li 
                         key={item}
-                        className={`uppercase cursor-pointer p-2 rounded-md  ${isActive === item ? active : "hover:bg-indigo-800"}`}
+                        className={`uppercase cursor-pointer p-2 ml-2 rounded-md  ${isActive === item ? active : "hover:bg-indigo-800"}`}
                         onClick={() => handleItemClick(item)}
                         >{item}</li>
                     )}
                 </ul>
             </nav>
-        </header>
+        </>
     )
 }
 
