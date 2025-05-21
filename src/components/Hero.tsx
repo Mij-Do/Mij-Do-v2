c9929aff482257bc71b220db08c5afa4d59fe067
ahmed <ahmedmijdo2@gmail.com>
2025-05-21 17:05:31 +0300

    add header effect component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,15 @@
 import { SiGithub, SiLinkedin } from "react-icons/si";
 import HeroImage from './../assets/HeroImage.png'
+import TypingText from "./ui/HeaderEffect";
 
 const Hero = () => {
     return (
         <section className="flex flex-col md:flex-row justify-between items-center bg-indigo-300 rounded-md my-15 md:space-x-40">
             <div className='text-center md:text-left'>
-                <h1 className="text-white bg-indigo-800 rounded-md p-2 mb-5 text-3xl">HI, I AM A DEVELOPER..</h1>
+                <TypingText
+                    texts={["HI, I AM A DEVELOPER..", "FRONTEND DEVELOPER", "REACT JS DEVELOPER"]}
+                />
+                {/* <h1 className="text-white bg-indigo-800 rounded-md p-2 mb-5 text-3xl">HI, I AM A DEVELOPER..</h1> */}
                 <p>
                     Front-end developer passionate about building clean, responsive, and user-friendly web interfaces using HTML, CSS, JavaScript, React, and TypeScript.
                 </p>
---
+++ src/components/ui/HeaderEffect.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+
+interface TypingTextProps {
+texts: string[];
+typingSpeed?: number;     
+deletingSpeed?: number;         
+}
+
+const TypingText = ({
+texts,
+typingSpeed = 100,
+deletingSpeed = 50,
+}: TypingTextProps) => {
+const [text, setText] = useState("");          
+const [isDeleting, setIsDeleting] = useState(false); 
+const [loopNum, setLoopNum] = useState(0);     
+const [delta, setDelta] = useState(typingSpeed); 
+
+useEffect(() => {
+    const fullText = texts[loopNum % texts.length];
+
+    const timer = setTimeout(() => {
+    setText((prev) =>
+        isDeleting ? prev.slice(0, -1) : fullText.substring(0, prev.length + 1)
+    );
+
+    
+    if (!isDeleting && text === fullText) {
+        setDelta(deletingSpeed);
+        setTimeout(() => setIsDeleting(true), 500);
+    } else if (isDeleting && text === "") {
+        setIsDeleting(false);
+        setDelta(typingSpeed);
+        setLoopNum((prev) => prev + 1);
+    }
+    }, delta);
+
+    return () => clearTimeout(timer);
+}, [text, isDeleting, loopNum, texts, typingSpeed, deletingSpeed]);
+
+return (
+    <h1 className="text-white bg-indigo-800 rounded-md p-2 mb-5 text-3xl">
+        {text}
+        <span className="inline-block w-1 h-8 bg-white ml-1 animate-pulse"></span>
+    </h1>
+);
+};
+
+export default TypingText;
