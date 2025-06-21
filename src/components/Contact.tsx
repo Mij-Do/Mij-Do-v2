965ddd4310ea77f5c224933c193b74adc98305c8
ahmed <ahmedmijdo2@gmail.com>
2025-06-21 17:35:51 +0300

    enhance contact layout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,6 @@
 import { dataInputs } from "../data";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-import { SiGmail } from "react-icons/si";
 
 
 
@@ -17,11 +16,8 @@ const Contact = () => {
     return (
         <section className="my-15">
             <h1 className="text-white text-center bg-indigo-950 w-full md:w-lg rounded-md p-2 mb-5 mx-auto text-3xl">Contact Me</h1>
-            <div className="flex flex-col md:flex-row w-full justify-evenly items-center md:space-x-5 md:space-y-0 space-y-5 space-x-0">
-                <div>
-                    <SiGmail size={200} className="bg-indigo-800 rounded-md p-2" color="white"/>
-                </div>
-                <form className="w-full md:w-sm">
+            <div className="flex w-full justify-evenly items-center">
+                <form className="w-full md:w-lg">
                     {renderInputs}
                     <Button className="text-white text-center hover:bg-indigo-950 w-full md:w-lg rounded-md p-2 mb-5 mx-auto text-lg transition">SEND MAIL</Button>
                 </form>
