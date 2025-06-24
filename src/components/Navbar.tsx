fba33eea25384346520ce68ecd6090b05b128e39
ahmed <ahmedmijdo2@gmail.com>
2025-06-24 17:09:38 +0300

    enhance the header navbar v3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { HiOutlineViewList } from 'react-icons/hi';
+import { HiX } from 'react-icons/hi';
+
 
 interface IProps {
     navItems: string[];
@@ -10,7 +12,8 @@ const Navbar = ({navItems, setPage}: IProps) => {
     const active: string = "bg-indigo-800";
     // states
     const [isActive, setActive] = useState('hero');
-    const [open, setOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
 
 
     // handellers
@@ -20,14 +23,15 @@ const Navbar = ({navItems, setPage}: IProps) => {
     };
 
     const handelOpen = () => {
-        setOpen(!open);
+        setIsOpen(!isOpen);
     }
+
 
     // render
     const renderNavItems = navItems.map(item => 
         <li 
             key={item}
-            className={`uppercase cursor-pointer p-2 md:ml-2 rounded-md  ${isActive === item ? active : "hover:bg-indigo-800"}`}
+            className={`uppercase cursor-pointer p-2 md:ml-2 mb-2 rounded-md  ${isActive === item ? active : "hover:bg-indigo-800"}`}
             onClick={() => handleItemClick(item)}
         >{item}</li>
     );
@@ -36,8 +40,13 @@ const Navbar = ({navItems, setPage}: IProps) => {
         <div className="flex flex-col w-full text-center md:flex-row md:justify-between">
             <h1 className="uppercase font-bold cursor-pointer bg-indigo-800 p-2 mb-2 rounded-md">mijdo</h1>
             <nav> 
-                <HiOutlineViewList onClick={handelOpen} size={40} className={`bg-indigo-800 rounded-md cursor-pointer md:hidden m-auto mb-5`}/>
-                <ul className={`${open === true ? 'flex' : 'hidden'} flex-col md:flex md:flex-row`}>
+                {isOpen === false ? <HiOutlineViewList onClick={handelOpen} 
+                                    size={42} 
+                                    className={`bg-indigo-800 rounded-md cursor-pointer md:hidden m-auto mb-5`}/> : 
+                                    <HiX onClick={handelOpen} 
+                                    size={42} 
+                                    className={`bg-indigo-800 rounded-md cursor-pointer md:hidden m-auto mb-5`}/>}
+                <ul className={`${isOpen === true ? 'flex' : 'hidden'} flex-col md:flex md:flex-row`}>
                     {renderNavItems}
                 </ul>
             </nav>
