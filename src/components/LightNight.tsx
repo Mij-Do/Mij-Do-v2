e06d8270107c5eee3aceb9f52c3b4a300e3baede
ahmed <ahmedmijdo2@gmail.com>
2025-07-29 08:46:11 +0300

    Error PageNotFound

--- src/components/LightNight.tsx
+++ src/components/LightNight.tsx
@@ -3,7 +3,7 @@ import styles from '../LightningBackground.module.css';
 const LightningBackground = () => {
     return (
         <>
-            <div className={styles.wrapper}>
+            <div className={styles.wrapperDark}>
                 <div className={styles.stars}></div>
                 <div className={styles.lightning}></div>
             </div>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,15 +4,21 @@ import About from '../pages/About';
 import Projects from '../pages/Projects';
 import Contact from '../pages/Contact';
 import RootLayout from '../pages/RootLayout';
+import PageNotFound from '../errors/PageNotFound';
 
 
 const router = createBrowserRouter(createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
-        <Route index element={<Hero />}/>
-        <Route path='about' element={<About />}/>
-        <Route path='projects' element={<Projects />}/>
-        <Route path='contact' element={<Contact />}/>
-    </Route>
+    <>
+        {/* Root layout */}
+        <Route path='/' element={<RootLayout />}>
+            <Route index element={<Hero />}/>
+            <Route path='about' element={<About />}/>
+            <Route path='projects' element={<Projects />}/>
+            <Route path='contact' element={<Contact />}/>
+        </Route>
+        {/* page not found error */}
+        <Route path='*' element={<PageNotFound />} />
+    </>
 ))
 
 export default router;
