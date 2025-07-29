63e6faa55b3574deb7a5ddce9d3e2dc46509e6da
ahmed <ahmedmijdo2@gmail.com>
2025-07-29 08:52:15 +0300

    Remove bar from skills card

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -28,14 +28,14 @@ const About = () => {
             <div className="w-full text-white">
                 <hr className="w-1/2 m-auto my-20"/>
                 <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-                    <CardSkills title="Html5" color="bg-orange-400" precntage="100%"><SiHtml5 size={48} className="bg-orange-400 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
-                    <CardSkills title="Css3" color="bg-blue-400" precntage="98%"><SiCss3 size={48} className="bg-blue-400 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
-                    <CardSkills title="Java Script" color="bg-yellow-400" precntage="91%"><SiJavascript size={48} className="bg-yellow-400 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
-                    <CardSkills title="Type Script" color="bg-indigo-800" precntage="81%"><SiTypescript size={48} className="bg-indigo-800 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
-                    <CardSkills title="ReactJS" color="bg-indigo-900" precntage="72%"><SiReact size={48} className="bg-indigo-900 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
-                    <CardSkills title="BootStrap" color="bg-purple-600" precntage="94%"><SiBootstrap size={48} className="bg-purple-600 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
-                    <CardSkills title="TailWind Css" color="bg-blue-700" precntage="87%"><SiTailwindcss size={48} className="bg-blue-700 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
-                    <CardSkills title="Sass" color="bg-pink-600" precntage="78%"><SiSass size={48} className="bg-pink-600 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="Html5"><SiHtml5 size={48} className="bg-orange-400 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="Css3"><SiCss3 size={48} className="bg-blue-400 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="Java Script"><SiJavascript size={48} className="bg-yellow-400 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="Type Script"><SiTypescript size={48} className="bg-indigo-800 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="ReactJS"><SiReact size={48} className="bg-indigo-900 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="BootStrap"><SiBootstrap size={48} className="bg-purple-600 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="TailWind Css"><SiTailwindcss size={48} className="bg-blue-700 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="Sass"><SiSass size={48} className="bg-pink-600 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
                 </div>
             </div>
         </section>
