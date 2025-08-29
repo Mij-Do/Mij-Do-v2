bb599bd63b9ee0215bd72c80dd28c6bc6e4c7b7b
ahmed <ahmedmijdo2@gmail.com>
2025-08-29 12:30:59 +0300

    update my cv link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <>
             <div className="text-center text-white">
                 <div className='flex justify-center items-center space-x-4'>
-                    <a href="https://drive.google.com/file/d/1ia6PQucjeNR3ctQ30tj67URYo7X2U6Ld/view" target="_blank" className="bg-indigo-800 hover:bg-indigo-600 border-2 border-white p-2 rounded-md cursor-pointer transition">CV</a>
+                    <a href="https://drive.google.com/file/d/1pkWWNH49z2q837hOD2MJPDQbY670yA5r/view?usp=sharing" target="_blank" className="bg-indigo-800 hover:bg-indigo-600 border-2 border-white p-2 rounded-md cursor-pointer transition">CV</a>
                 </div>
                 <p>Created By &copy; Ahmed Samir </p>
             </div>
