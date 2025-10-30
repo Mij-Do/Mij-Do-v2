3ecfc5c79700db48840133436b1bd64778e2f4fb
ahmed <ahmedmijdo2@gmail.com>
2025-10-30 14:42:02 +0300

    new about me description && new skills

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import { SiJavascript, SiHtml5, SiCss3, SiReact, SiBootstrap, SiTailwindcss, SiSass, SiTypescript } from "react-icons/si";
+import { SiJavascript, SiHtml5, SiCss3, SiReact, SiBootstrap, SiTailwindcss, SiSass, SiTypescript, SiRedux, SiAxios } from "react-icons/si";
 import CardSkills from "../components/ui/CardSkills";
 
 const About = () => {
@@ -8,15 +8,13 @@ const About = () => {
             <h1 className="text-center bg-indigo-950 w-full md:w-lg rounded-md p-2 mb-5 mx-auto text-3xl">About Me</h1>
             <div className="flex flex-col justify-between items-center text-center  p-2 md:flex-row space-x-10">
                 <p className="w-fit md:mx-2">
-                    Hi, I'm Ahmed — a passionate Front-End Developer who builds modern, responsive, and user-friendly web interfaces. I specialize in turning creative ideas into functional and elegant websites using a solid tech stack that includes:
+                    Hi, I’m Ahmed — a passionate Front-End Developer who builds modern, responsive, and user-friendly web applications. I specialize in turning creative ideas into functional and elegant digital experiences using a solid tech stack that includes: HTML | CSS | JavaScript | React JS | TypeScript | Tailwind CSS | SASS | Bootstrap.<br />
 
-                    HTML | CSS | JavaScript | React JS | TypeScript | Tailwind CSS | SASS | Bootstrap
+                    I’ve completed my learning journey in React and TypeScript, and I’m currently expanding my skills into Node.js as I explore the back-end side of development.<br />
 
-                    I'm currently focused on mastering React and TypeScript, and I enjoy working with Tailwind CSS and SASS to create clean, maintainable UI components. I pay strong attention to detail, and I always aim to write scalable code that performs well across all devices.
+                    In my projects, I work with modern libraries and tools such as Axios, React Router DOM, TanStack Query, and Redux Toolkit to create efficient, scalable, and maintainable applications. I care deeply about clean code, performance optimization, and delivering seamless user experiences across all devices. <br />
 
-                    I'm continuously learning, always experimenting with new tools and frameworks, and committed to building projects that make a real difference.
-
-                    Outside of coding, I love improving my English, exploring new technologies, and working on personal projects that challenge my creativity and logic.
+                    I’m always learning, experimenting with new technologies, and building projects that solve real problems. Outside of coding, I love improving my English, exploring new frameworks, and challenging myself with creative side projects that push my technical and logical thinking further.
                 </p>
             </div>
 
@@ -32,6 +30,8 @@ const About = () => {
                     <CardSkills title="BootStrap"><SiBootstrap size={48} className="bg-purple-600 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
                     <CardSkills title="TailWind Css"><SiTailwindcss size={48} className="bg-blue-700 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
                     <CardSkills title="Sass"><SiSass size={48} className="bg-pink-600 rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="Axios"><SiAxios size={48} className="bg-[#5a29e4] rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
+                    <CardSkills title="Redux-Toolkit"><SiRedux size={48} className="bg-[#593d88] rounded-md hover:scale-95 transition-all mb-5"/></CardSkills>
                 </div>
             </div>
         </section>
